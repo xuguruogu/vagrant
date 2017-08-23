52871c177ae24ec3262691da1e00f827ea9d42f1
kent <06libenli@163.com>
2017-08-23 13:17:41 +0800

    update

--- scripts/fedora-init.sh
+++ scripts/fedora-init.sh
@@ -7,7 +7,7 @@ dnf install -y libaio-devel hwloc-devel numactl-devel libpciaccess-devel cryptop
 dnf install -y gcc-c++ ninja-build ragel boost-devel libubsan xen-devel scons glibc-static libstdc++-static
 dnf install -y subversion git corkscrew redis gdb-gdbserver ruby
 dnf update -y vim-minimal
-dnf install -y vim lsof tcpdump dnf-plugins-core gdb jemalloc-devel gperftools-libs wget telnet tcl java shtool
+dnf install -y vim lsof tcpdump dnf-plugins-core gdb jemalloc-devel gperftools-libs wget telnet tcl java shtool zsh
 
 mkdir /home/coredump
 chmod 777 /home/coredump
@@ -29,6 +29,23 @@ export PS1="\[\e[37;40m\][\[\e[32;40m\]\u\[\e[37;40m\]@\h \[\e[36;40m\]\w\[\e[0m
 export ETCDCTL_API=3
 export BLADE_AUTO_UPGRADE=no
 
+export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
+EOF
+
+# change to zsh
+chsh -s /bin/zsh vagrant
+su - vagrant -c 'sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"'
+su - vagrant -c 'if [ -f ~/.zshrc -a ! -f ~/.zshrc.bak ]; then cp ~/.zshrc ~/.zshrc.bak; fi'
+cat > /home/vagrant/.zshrc << EOF
+export ZSH=\$HOME/.oh-my-zsh
+ZSH_THEME="robbyrussell"
+plugins=(git golang redis-cli extract z autojump colored-man-pages)
+source $ZSH/oh-my-zsh.sh
+
+export HADOOP_HOME=/usr/local/hadoop
+export PATH=\$PATH:\$HOME/.local/bin:\$HOME/bin:\$HADOOP_HOME/bin
+export ETCDCTL_API=3
+export BLADE_AUTO_UPGRADE=no
 export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
 EOF
 
